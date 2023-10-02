770ed02b957fd6af82b90343ef9e7fdbd6b48165
Sarvesh207 <sarveshgaynar@gmail.com>
2023-10-02 06:42:49 +0530

    1st problem on pattern

---
+++ Patterns/pattern1.js
@@ -0,0 +1,13 @@
+let n = 3;
+
+function printPattern(n) {
+    for(let row = 1; row <= n; row++){
+        let str = "";
+        for (let col = 1; col <= n; col++){
+            str += "*";
+        }
+        console.log(str);
+    }
+}
+
+printPattern(n);
