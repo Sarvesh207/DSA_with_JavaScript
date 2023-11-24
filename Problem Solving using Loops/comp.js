4e3d27248eef26a486feab1867418d7000b02b02
Sarvesh207 <sarveshgaynar@gmail.com>
2023-11-24 23:44:46 +0530

    started dsa

---
+++ Problem Solving using Loops/comp.js
@@ -0,0 +1,13 @@
+let x = "abc";
+let y = "abc";
+// console.log(x === y);
+// console.log(typeof x)
+// console.log(typeof y)
+
+
+let z = String("abc");
+let a = new String("abc");
+
+console.log(x === z)
+// console.log(typeof z)
+// console.log(typeof a)
---
+++ Problem Solving using Loops/gcd.js
@@ -0,0 +1,12 @@
+function gcd(a, b){
+    let ans = 1;
+    for(let i=2; i<=Math.min(a, b); i++){
+        if(a % i == 0 && b % i == 0){
+            ans = i;
+        }
+    }
+    return ans;
+}
+
+const ans = gcd(24, 28);
+console.log("The gcd is" , ans)
