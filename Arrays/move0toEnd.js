dcc4f747bace495dff8cd7260492bbaaa2b6fd39
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-03 23:56:04 +0530

    day 1

---
+++ Arrays/move0toEnd.js
@@ -0,0 +1,10 @@
+/**
+ * Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+ */
+function moveZerosToEnd(arr) {
+    let index = 0;
+    for(let i=0; i<arr.length, i++){
+        
+    }
+}
---
+++ Objects/loopingonObject.js
@@ -0,0 +1,16 @@
+let x = {
+    name:"john",
+    company:"microsoft",
+    salary:"20000",
+    age:40,
+    city:"bangluru",
+}
+
+let keysOfX = Object.keys(x); // will return an array of keys of x 
+// console.log(keysOfX)
+
+let valuesOfX = Object.values(x)   // will return an array of value of x
+// console.log(valuesOfX)
+
+let entriesOfx = Object.entries(x)
+console.log(entriesOfx)
