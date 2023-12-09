effbb49feb870d32d782186aba9c1a7c4cbecac6
Sarvesh207 <sarveshgaynar@gmail.com>
2023-12-09 23:34:12 +0530

    day 5

---
+++ Arrays/mergeTwoSortedArray.js
@@ -0,0 +1,10 @@
+function twoMergeSortedArray(nums1, m, nums2, n){
+    let result = Array(m+n).fill(0);
+    let i = 0;
+    let j = 0;
+    let k = 0
+
+    while(i < m && j < n){
+        if(nums1)
+    }
+}
