48d4e1c1a40952d38614c0bed19d55728d4eefa3
Sarvesh207 <sarveshgaynar@gmail.com>
2024-01-19 09:06:05 +0530

    back to dsa

---
+++ Strings/reverse.js
@@ -0,0 +1,10 @@
+function reverseWord(str){
+    const strArray = str.split();
+    
+}
+
+
+
+const str1 = "Welcome to this Javascript Guide!"
+const result = reverseWord(str1)
+console.log(result)
