7298ed33fcdc434ce2ea7e73c6b4b0095e243391
Sarvesh207 <sarveshgaynar@gmail.com>
2024-02-11 23:56:16 +0530

    recursion

---
+++ recursion/factorial_recusion.js
@@ -0,0 +1,19 @@
+// function f(n) {
+//     if (n === 1) return 1;
+
+//     return n * f(n - 1);
+// }
+
+// console.log(f(5));
+
+function factorical(n) {
+
+    let facto = 1;
+    for(let i=n; i>0; i--){
+        facto  *= i
+    }
+
+    return facto;
+}
+
+console.log(factorical(5))
