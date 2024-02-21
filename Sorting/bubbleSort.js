772f820c4495e69ab4253d1ca414e23d31e25ead
Sarvesh207 <sarveshgaynar@gmail.com>
2024-02-21 22:57:28 +0530

    bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+
+}
+
+let arr = [1, 2, -3, 12, 6];
+
+console.log(arr);
+
+bubbleSort(arr)
+console.log(arr)
