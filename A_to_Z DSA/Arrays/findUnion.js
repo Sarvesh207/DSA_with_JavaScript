07965812a276515f462e1e4e8f30cde75e4b897c
Sarvesh Gaynar <109072302+Sarvesh207@users.noreply.github.com>
2024-03-14 16:44:08 +0000

    union of array brutforce

---
+++ A_to_Z DSA/Arrays/findUnion.js
@@ -0,0 +1,40 @@
+// function findUnionofArray(a, b){
+
+//     let union = new Set();
+    
+//     for(let i=0;  i<a.length; i++){
+
+//         union.add(a[i])
+
+//     }
+
+//     for(let i=0;  i<b.length; i++){
+
+//         union.add(b[i])
+
+//     }
+
+//     console.log(union)
+
+// }
+
+// let arr1 = [1,2,3,4,5];
+// let arr2 = [2,3,4,4,5];
+
+// findUnionofArray(arr1, arr2)
+
+
+//  optimal approch
+
+function unionArray(a, b){
+    let n = a.length;
+    let m = b.length;
+    let i = j = 0
+    while(i < a.length && b.length){
+        
+    }
+
+
+}
+
+unionArray()
