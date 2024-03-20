7d1358e48fced2366116df75c0fa2136035e4446
Sarvesh207 <sarveshgaynar@gmail.com>
2024-03-20 19:52:59 +0530

    some changes

--- A_to_Z DSA/Math/isPalindrome.js
+++ A_to_Z DSA/Math/isPalindrome.js
@@ -1,20 +1,43 @@
+// function isNumPalindrome(num) {
+//   let revNum = 0;
+//   let originalNum = num; // To store a copy of the original number
+
+//   while (num > 0) {
+//     let digit = num % 10;
+
+//     revNum = revNum * 10 + digit;
+//     console.log(revNum);
+
+//     num = Math.floor(num / 10); // Using integer division to get the next number
+//   }
+
+//   // Now, after the while loop, compare the reversed number with the original number
+//   if (originalNum === revNum) {
+//     return true; // It's a palindrome
+//   } else {
+//     return false; // It's not a palindrome
+//   }
+// }
+
+// let num = 1213;
+// console.log(isNumPalindrome(num));
 
 function isNumPalindrome(num) {
-  let revNum = 0;
-  let originalNum = num; // To store a copy of the original number
+	let revNum = 0;
+	let originalNum = num; // To store a copy of the original number
 
-  while (num > 0) {
+while (num > 0) {
     let digit = num % 10;
-    
+
     revNum = revNum * 10 + digit;
     console.log(revNum);
-    
+
     num = Math.floor(num / 10); // Using integer division to get the next number
   }
-  
+
   // Now, after the while loop, compare the reversed number with the original number
   if (originalNum === revNum) {
-    return true; // It's a palindrome
+return true; // It's a palindrome
   } else {
     return false; // It's not a palindrome
   }
