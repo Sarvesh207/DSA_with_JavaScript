107e7668a43720887d57ea2b6ca8b24923d18364
Sarvesh207 <sarveshgaynar@gmail.com>
2024-10-14 23:49:21 +0530

    test git

--- 3 - months of dsa/01.Oct-2024/sorted&rotaed.js
+++ 3 - months of dsa/01.Oct-2024/sorted&rotaed.js
@@ -1,18 +1,20 @@
 function check(nums) {
     let countRotations = 0;
     let n = nums.length;
-    
+
     for (let i = 0; i < n; i++) {
         // Check if the current element is greater than the next element
         if (nums[i] > nums[(i + 1) % n]) {
             countRotations++;
         }
-        
+
         // If there is more than one "rotation point", return false
         if (countRotations > 1) {
             return false;
         }
     }
-    
+
     return true;
 }
+
+//  Optimize approch
