07f1f5cb1733c362a626cecf7fc27ece38a38fb0
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-07 23:38:23 +0530

    minar changes

--- 2025/Jan/Contains_Duplicate.js
+++ 2025/Jan/Contains_Duplicate.js
@@ -3,33 +3,30 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
+  //  brute force solution
+  //   let isDublicate = false;
+  //   for (let i = 0; i < nums.length; i++) {
+  //     for (let j = i + 1; j < nums.length; j++) {
+  //       if (nums[i] == nums[j]) {
+  //         isDublicate = true;
+  //         return isDublicate;
+  //       }
+  //     }
+  //   }
 
-    //  brute force solution
-//   let isDublicate = false;
-//   for (let i = 0; i < nums.length; i++) {
-//     for (let j = i + 1; j < nums.length; j++) {
-//       if (nums[i] == nums[j]) {
-//         isDublicate = true;
-//         return isDublicate;
-//       }
-//     }
-//   }
+  //   return false;
 
-//   return false;
+  //  optimal solution
 
+  const elementTracker = {};
 
-
-//  optimal solution
-
-    const elementTracker = {};
-
-    for( let num of nums){
-        if(elementTracker[num]){
-            return true;
-        }
-
-        elementTracker[num] = true;
+  for (let num of nums) {
+    if (elementTracker[num]) {
+      return true;
     }
 
-    return false;
+    elementTracker[num] = true;
+  }
+
+  return false;
 };
