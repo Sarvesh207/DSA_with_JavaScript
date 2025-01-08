d21d0c85378dbc6cf80623478d31f09c458ba05a
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-08 23:19:27 +0530

    optimal contains dublicate 2 leetcode Problem 219

--- 2025/Jan/containerDublicate2.js
+++ 2025/Jan/containerDublicate2.js
@@ -1,8 +1,21 @@
-var containsNearbyDuplicate = function(nums, k) {
-    for(let i=0; i<=nums.length; i++){
-        for(let j=i+1; j<=nums.length; j++){
-            if(nums[i] === nums[j] && Math.abs(i-j) <= k) return true;
-        }
+// var containsNearbyDuplicate = function(nums, k) {
+//     for(let i=0; i<=nums.length; i++){
+//         for(let j=i+1; j<=nums.length; j++){
+//             if(nums[i] === nums[j] && Math.abs(i-j) <= k) return true;
+//         }
+//     }
+//     return false;
+// };
+
+
+for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && Math.abs(i - map.get(nums[i])) <= k) {
+        return true; // found a duplicate within the range
     }
-    return false;
-};
+    map.set(nums[i], i); // store the index of the current element
+
+    // Remove elements that are more than k distance away from the current index
+    if (i >= k) {
+        map.delete(nums[i - k]);
+    }
+}
