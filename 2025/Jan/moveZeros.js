6f3971545d38e6530d5258315c9c71904eea0e81
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-11 08:43:51 +0530

    move zeros to end optimal solution

--- 2025/Jan/moveZeros.js
+++ 2025/Jan/moveZeros.js
@@ -2,22 +2,47 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    const newArray = [];
-    for(let i=0; i<nums.length; i++){
-        if(nums[i] !== 0){
-            newArray.push(nums[i]);
-        }
+var moveZeroes = function (nums) {
+  const newArray = [];
+  // for(let i=0; i<nums.length; i++){
+  //     if(nums[i] !== 0){
+  //         newArray.push(nums[i]);
+  //     }
+  // }
+
+  // let nz = newArray.length;
+
+  // for(let i=0; i<nz; i++){
+  //     nums[i] = newArray[i];
+  // }
+
+  // for(let i=nz; i<nums.length; i++){
+  //     nums[i] = 0;
+  // }
+
+  //  allocate j to first zero element
+
+  let j = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 0) {
+      j = i;
+      break;
     }
+  }
 
+  if (j === -1) return nums;
 
-    let nz = newArray.length;
-
-    for(let i=0; i<nz; i++){
-        nums[i] = newArray[i];
+  for (let i = j + 1; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      // when nums[i] will be non zero element
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      j++;
     }
+  }
 
-    for(let i=nz; i<nums.length; i++){
-        nums[i] = 0;
-    }
+  return nums;
 };
+
+let nums = [0, 1, 0, 3, 1, 2];
+
+console.log(moveZeroes(nums));
