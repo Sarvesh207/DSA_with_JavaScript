597d5c7469008b2481fcc3777cee3ff4d98230c2
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-28 22:52:45 +0530

    refactor moveZeroes function to remove commented-out code and improve readability

--- 2025/Jan/moveZeros.js
+++ 2025/Jan/moveZeros.js
@@ -4,21 +4,21 @@
  */
 var moveZeroes = function (nums) {
   const newArray = [];
-  // for(let i=0; i<nums.length; i++){
-  //     if(nums[i] !== 0){
-  //         newArray.push(nums[i]);
-  //     }
-  // }
+  for(let i=0; i<nums.length; i++){
+      if(nums[i] !== 0){
+          newArray.push(nums[i]);
+      }
+  }
 
-  // let nz = newArray.length;
+  let nz = newArray.length;
 
-  // for(let i=0; i<nz; i++){
-  //     nums[i] = newArray[i];
-  // }
+  for(let i=0; i<nz; i++){
+      nums[i] = newArray[i];
+  }
 
-  // for(let i=nz; i<nums.length; i++){
-  //     nums[i] = 0;
-  // }
+  for(let i=nz; i<nums.length; i++){
+      nums[i] = 0;
+  }
 
   //  allocate j to first zero element
 
