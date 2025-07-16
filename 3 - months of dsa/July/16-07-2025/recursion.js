dd6c6da846f953c4e77e8bbb700a71171796078f
Sarvesh207 <sarveshgaynar@gmail.com>
2025-07-16 22:26:42 +0530

    sum of all array elements and sum of odd elements from the array

--- 3 - months of dsa/July/16-07-2025/recursion.js
+++ 3 - months of dsa/July/16-07-2025/recursion.js
@@ -6,3 +6,26 @@ function sum(n) {
 }
 
 console.log(sum(5));
+
+//  Sum for all elements of array
+function sum1(arr, n) {
+  if (n === 0) return arr[0];
+
+  return arr[n] + sum(arr, n - 1);
+}
+
+let arr = [5, 3, 2, 6, 1];
+let n = arr.length - 1;
+console.log(sum(arr, n));
+
+//  Sum for all elements of array
+function sum1(arr, n) {
+  let isOdd = arr[n] % 2 !== 0;
+  if (n === 0) return isOdd ? arr[0] : 0;
+
+  return isOdd ? arr[n] : 0 + sum(arr, n - 1);
+}
+
+let arr1 = [5, 3, 2, 6, 1];
+let n1 = arr.length - 1;
+console.log(sum(arr1, n1));
