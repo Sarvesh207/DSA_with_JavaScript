1c70d78698a6881a3ad860fa188706913ba34ce7
Sarvesh207 <sarveshgaynar@gmail.com>
2025-09-18 22:44:52 +0530

    balence string both approch

---
+++ 3 - months of dsa/sept/balenceString.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+  let R = 0;
+  let L = 0;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "R") R++;
+    else L++;
+
+    if (L === R) {
+      R = 0;
+      L = 0;
+      count++;
+    }
+  }
+
+  return count;
+};
+
+
+
+
+//  Other Approch  
+
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    temp = 0
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+
+        if (s[i] == "R") temp++
+        else temp--;
+
+        if (temp === 0) {
+
+
+            count++
+        }
+
+    }
+
+    return count;
+};
